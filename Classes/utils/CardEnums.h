81008b6834547b19462d1abef414b54a35742879
jiran <1781415302@qq.com>
2025-12-06 20:26:57 +0800

    工具注释修复

--- Classes/utils/CardEnums.h
+++ Classes/utils/CardEnums.h
@@ -8,10 +8,10 @@
 enum class CardSuitType : int
 {
     CST_NONE = -1,
-    CST_CLUBS,      // 梅花
-    CST_DIAMONDS,   // 方块
-    CST_HEARTS,     // 红桃
-    CST_SPADES,     // 黑桃
+    CST_CLUBS,      // 梅花=0
+    CST_DIAMONDS,   // 方块=1
+    CST_HEARTS,     // 红桃=2
+    CST_SPADES,     // 黑桃=3
     CST_NUM_CARD_SUIT_TYPES
 };
 
@@ -19,19 +19,19 @@ enum class CardSuitType : int
 enum class CardFaceType : int
 {
     CFT_NONE = -1,
-    CFT_ACE,    // 1
-    CFT_TWO,    // 2
-    CFT_THREE,  // 3
-    CFT_FOUR,   // 4
-    CFT_FIVE,   // 5
-    CFT_SIX,    // 6
-    CFT_SEVEN,  // 7
-    CFT_EIGHT,  // 8
-    CFT_NINE,   // 9
-    CFT_TEN,    // 10
-    CFT_JACK,   // 11
-    CFT_QUEEN,  // 12
-    CFT_KING,   // 13
+	CFT_ACE,    // A=0
+    CFT_TWO,    // 2=1
+    CFT_THREE,  // 3=2
+    CFT_FOUR,   // 4=3
+    CFT_FIVE,   // 5=4
+    CFT_SIX,    // 6=5
+	CFT_SEVEN,  // 7=6
+    CFT_EIGHT,  // 8=7
+    CFT_NINE,   // 9=8
+    CFT_TEN,    // 10=9
+	CFT_JACK,   // J=10
+    CFT_QUEEN,  // Q=11
+    CFT_KING,   // K=12
     CFT_NUM_CARD_FACE_TYPES
 };
 
